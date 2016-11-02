a986dfb7405e134b45e8617406ef66ba3430809c
Dominic Zirbel <djynth@gmail.com>
2016-11-02 13:14:04 -0400

    added custom CSS for cover letters

--- src/cover_letters/compile.sh
+++ src/cover_letters/compile.sh
@@ -9,6 +9,7 @@ function compile {
     RESUME_CSS=`lessc ../resume.less --clean-css="--s0 --advanced" | sed -f ../parse-apos.sed`
     CONTAINER_CSS=`lessc ../container.less --clean-css="--s0 --advanced" | sed -f ../parse-apos.sed`
     PDF_CSS=`lessc ../pdf.less --clean-css="-s0 --advanced" | sed -f ../parse-apos.sed`
+    COVER_LETTER_CSS=`lessc cover_letter.less --clean-css="-s0 --advanced" | sed -f ../parse-apos.sed`
 
     for file in *.jade
     do
@@ -16,8 +17,8 @@ function compile {
         filename=$(basename "$file")
         filename="${filename%.*}"
 
-        pug --obj "{ 'css' : '$RESUME_CSS$CONTAINER_CSS', 'content' : '$COVER_LETTER', 'container' : true,  'title' : '$TITLE' }" < ../container.jade > $filename.html
-        pug --obj "{ 'css' : '$RESUME_CSS$PDF_CSS',       'content' : '$COVER_LETTER', 'container' : false, 'title' : '$TITLE' }" < ../container.jade > pdf.html
+        pug --obj "{ 'css' : '$COVER_LETTER_CSS$RESUME_CSS$CONTAINER_CSS', 'content' : '$COVER_LETTER', 'container' : true,  'title' : '$TITLE' }" < ../container.jade > $filename.html
+        pug --obj "{ 'css' : '$COVER_LETTER_CSS$RESUME_CSS$PDF_CSS',       'content' : '$COVER_LETTER', 'container' : false, 'title' : '$TITLE' }" < ../container.jade > pdf.html
 
         if hash wkhtmltopdf 2>/dev/null; then
             wkhtmltopdf -q --page-size Letter --dpi 96 --title "$TITLE" pdf.html $filename.pdf
